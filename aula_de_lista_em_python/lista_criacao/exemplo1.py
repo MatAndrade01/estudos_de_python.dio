9876dfb82aa1883b2394d37b35cc5172a9e77fec
Matheus Andrade <125470760+MatAndrade01@users.noreply.github.com>
2024-05-01 18:11:50 -0300

    Começando a ver listas!

---
+++ aula_de_lista_em_python/lista_criacao/exemplo1.py
@@ -0,0 +1,14 @@
+frutas = ['Laranja', 'Maça' , 'Uva']
+print(frutas)
+
+frutas = []
+print(frutas)
+
+letras = list('Python')
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ['Ferrari', 'F8', 42000, 2020, 2900, 'São Paulo', True]
+print(carro)
